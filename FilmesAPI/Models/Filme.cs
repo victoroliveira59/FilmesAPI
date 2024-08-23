d67d89ed376e2f7191132a00e5b1c6a11bb0bdc9
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2024-08-23 15:04:08 -0300

    Realizado a criação do Model e Adicionado Data Annotations

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,9 +1,17 @@
-﻿namespace FilmesAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
 
 public class Filme
 {
-
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
     public string Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage ="O tamanho do Titulo não pode ser maior de 50 caracteres")]
     public string Genero { get; set; }
+    [Required(ErrorMessage = "O diretor do filme é obrigatório")]
+    public string Diretor { get; set; }
+    [Required]
+    [Range(70,220,ErrorMessage ="A duração do filme deve ser de 70 a 220 minutos")]
     public int Duracao { get; set; }
 }
